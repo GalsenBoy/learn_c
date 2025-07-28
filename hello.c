f9686306e6edb23f34abae355808bb3cbcf46c95
GalsenBoy <bilaly.cissokho21@gmail.com>
2025-07-28 11:56:15 +0200

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int one = 70;
+    int two = 82;
+    int three = 185;
+    int four = 30;
+    char a = 'c';
+    char b = 'o';
+    char c = 'u';
+    char d = 'C';
+    char e = "O";
+    char f = 'U';
+    return 0;
+}
+
